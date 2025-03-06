d77c811137db403842643ed302247ed21133e261
MestreAutobot <viniciusmmaia22@gmail.com>
2025-03-06 16:16:24 -0300

    adicionei um menu de vendas de printf

--- passagensAereas.c
+++ passagensAereas.c
@@ -2,7 +2,22 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef struct{
+  
 
+}aeroporto;
+
+void menuVendas(){
+    printf("[O]---------------------------------[O]\n");
+    printf(" |  Bem vindo ao site linhas aéreas  |\n");
+    printf("[O]---------------------------------[O]\n");
+    printf(" |                                   |\n");
+    printf(" | [1] Configuracoes                 |\n");
+    printf(" | [2] Vendas                        |\n");
+    printf(" | [3] Sair                          |\n");
+    printf(" |                                   |\n");
+    printf("[O]---------------------------------[O]\n");
+}
 
 int main (){
 
