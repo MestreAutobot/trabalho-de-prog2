08ebe27a5f111f371b0f37316d298cdd1af2ebd5
MestreAutobot <viniciusmmaia22@gmail.com>
2025-03-06 16:18:07 -0300

    leve mudança no printf

--- passagensAereas.c
+++ passagensAereas.c
@@ -9,7 +9,7 @@ typedef struct{
 
 void menuVendas(){
     printf("[O]---------------------------------[O]\n");
-    printf(" |  Bem vindo ao site linhas aéreas  |\n");
+    printf(" |  Bem vindo ao site linhas aereas  |\n");
     printf("[O]---------------------------------[O]\n");
     printf(" |                                   |\n");
     printf(" | [1] Configuracoes                 |\n");
@@ -17,10 +17,11 @@ void menuVendas(){
     printf(" | [3] Sair                          |\n");
     printf(" |                                   |\n");
     printf("[O]---------------------------------[O]\n");
+    printf(" | Digite uma opcao acima: ");
 }
 
 int main (){
-
+    menuVendas();
 
 
     return 0;
